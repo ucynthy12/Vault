80fa90acdd929e995d836d78b3e7936e84357e09
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-11-13 14:16:40 +0200

    function save user

--- run.py
+++ run.py
@@ -9,3 +9,9 @@ def create_user(fname,lname,pswd,c_pswd):
     """
     new_user= User(fname,lname,pswd,c_pswd)
     return new_user
+
+def save_users(user):
+    """
+    Function to save user
+    """
+    user.save_users()
